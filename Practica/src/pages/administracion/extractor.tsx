cccd2c7ff4e3493852b91643bd0fe5d2345b2c00
natchor <ignacio.ibanez.alfaro@gmail.com>
2025-02-11 15:56:21 -0300

    final

--- Practica/src/pages/administracion/extractor.tsx
+++ Practica/src/pages/administracion/extractor.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { getUsers } from '@/Servicios/apiService';
 import { v4 as uuidv4 } from 'uuid';
+import NavBar from '@/componentes/NavBar';
 import '../styles/extractor.css';
 
 interface UserData {
@@ -36,7 +37,7 @@ const Extractor = () => {
   useEffect(() => {
     const user = localStorage.getItem('user');
     if (!user) {
-      router.push('/login');
+      router.push('/administracion/login');
     } else {
       const fetchUsers = async () => {
         setLoading(true);
@@ -92,58 +93,57 @@ const Extractor = () => {
   };
 
   return (
-    <div className="custom-container mx-auto p-4">
-      <div className="search-container mb-4">
-        <h2 className="text-2xl font-bold mb-4">Buscar Persona</h2>
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearch}
-          placeholder="Ingrese el nombre"
-          className="custom-input mb-4 p-2 border border-gray-300 rounded"
-        />
-        <button onClick={handleRefresh} className="custom-button bg-gray-500 text-white px-4 py-2 rounded">
-          Refrescar
-        </button>
-        {searchTerm && (
-          <div className="data-container">
-            {filteredUsers.length > 0 ? (
-              <ul className="user-list">
-                {filteredUsers.map(user => (
-                  <li key={user.id} onClick={() => handleSelectUser(user)} className="user-item">
-                    {user.name}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <div className="no-data">No se encontraron datos</div>
-            )}
+    <div>
+      <NavBar />
+      <div className="custom-container mx-auto p-4">
+        <div className="search-container mb-4">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearch}
+            placeholder="Ingrese el nombre"
+            className="custom-input mb-4 p-2 border border-gray-300 rounded"
+          />
+          {searchTerm && (
+            <div className="data-container">
+              {filteredUsers.length > 0 ? (
+                <ul className="user-list">
+                  {filteredUsers.map(user => (
+                    <li key={user.id} onClick={() => handleSelectUser(user)} className="user-item">
+                      {user.name}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <div className="no-data">No se encontraron datos</div>
+              )}
+            </div>
+          )}
+        </div>
+
+        {loading && <div className="loading">Cargando...</div>}
+        {error && <div className="error text-red-500">Error: {error}</div>}
+
+        {selectedUser && (
+          <div className="user-details mt-4 p-4 border border-gray-300 rounded bg-white shadow-md">
+            <h2 className="text-2xl font-bold mb-4">Detalles del Usuario</h2>
+            <p><strong>Nombre:</strong> {selectedUser.name}</p>
+            <p><strong>Username:</strong> {selectedUser.username}</p>
+            <p><strong>Email:</strong> {selectedUser.email}</p>
+            <p><strong>Teléfono:</strong> {selectedUser.phone}</p>
+            <p><strong>Website:</strong> {selectedUser.website}</p>
+            <p><strong>Dirección:</strong> {selectedUser.address.street}, {selectedUser.address.suite}, {selectedUser.address.city}, {selectedUser.address.zipcode}</p>
+            <p><strong>Compañía:</strong> {selectedUser.company.name}</p>
+            <p><strong>CatchPhrase:</strong> {selectedUser.company.catchPhrase}</p>
+            <p><strong>BS:</strong> {selectedUser.company.bs}</p>
           </div>
         )}
-      </div>
 
-      {loading && <div className="loading">Cargando...</div>}
-      {error && <div className="error text-red-500">Error: {error}</div>}
-
-      {selectedUser && (
-        <div className="user-details mt-4 p-4 border border-gray-300 rounded bg-white shadow-md">
-          <h2 className="text-2xl font-bold mb-4">Detalles del Usuario</h2>
-          <p><strong>Nombre:</strong> {selectedUser.name}</p>
-          <p><strong>Username:</strong> {selectedUser.username}</p>
-          <p><strong>Email:</strong> {selectedUser.email}</p>
-          <p><strong>Teléfono:</strong> {selectedUser.phone}</p>
-          <p><strong>Website:</strong> {selectedUser.website}</p>
-          <p><strong>Dirección:</strong> {selectedUser.address.street}, {selectedUser.address.suite}, {selectedUser.address.city}, {selectedUser.address.zipcode}</p>
-          <p><strong>Compañía:</strong> {selectedUser.company.name}</p>
-          <p><strong>CatchPhrase:</strong> {selectedUser.company.catchPhrase}</p>
-          <p><strong>BS:</strong> {selectedUser.company.bs}</p>
+        <div className="button-container mt-4">
+          <button onClick={handleGenerateQR} className="custom-button bg-green-500 text-white px-4 py-2 rounded">
+            Generar QR
+          </button>
         </div>
-      )}
-
-      <div className="button-container mt-4">
-        <button onClick={handleGenerateQR} className="custom-button bg-green-500 text-white px-4 py-2 rounded">
-          Generar QR
-        </button>
       </div>
     </div>
   );
--- Practica/src/pages/administracion/qr.tsx
+++ Practica/src/pages/administracion/qr.tsx
@@ -4,6 +4,7 @@ import QRCode from 'react-qr-code';
 import { getUserById } from '@/Servicios/apiService';
 import { v4 as uuidv4 } from 'uuid';
 import '../styles/qr.css';
+import NavBar from '@/componentes/NavBar';
 
 interface UserData {
   id: number;
@@ -53,7 +54,7 @@ const QRPage = () => {
     const newQrValue = `${window.location.origin}/ficha/webinfo?id=${userId}&uniqueId=${uniqueId}`;
     setQrValue(newQrValue);
     setQrStyle({
-            padding: '10px',
+      padding: '10px',
       borderRadius: '10px',
     });
   };
@@ -74,47 +75,51 @@ const QRPage = () => {
     console.log('Imprimir QR');
   };
 
-  
   const isBlocked = (url: string) => {
     return blockedUrls.includes(url);
   };
 
   return (
-    <div className="container">
-      <h2 className="text-2xl font-bold mb-4">QR Page</h2>
-      {user && (
-        <div className="user-details mt-4">
-          <h2 className="text-2xl font-bold mb-4">Detalles del Usuario</h2>
-          <p><strong>Nombre:</strong> {user.name}</p>
-          <p><strong>Username:</strong> {user.username}</p>
-          <p><strong>Email:</strong> {user.email}</p>
-          <p><strong>Teléfono:</strong> {user.phone}</p>
-          <p><strong>Website:</strong> {user.website}</p>
-          <p><strong>Dirección:</strong> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
-          <p><strong>Compañía:</strong> {user.company.name}</p>
-          <p><strong>CatchPhrase:</strong> {user.company.catchPhrase}</p>
-          <p><strong>BS:</strong> {user.company.bs}</p>
+    <div>
+      <NavBar />
+      <div className="custom-container mx-auto p-4">
+        <div className="container">
+          <h2 className="text-2xl font-bold mb-4">QR Page</h2>
+          {user && (
+            <div className="user-details mt-4">
+              <h2 className="text-2xl font-bold mb-4">Detalles del Usuario</h2>
+              <p><strong>Nombre:</strong> {user.name}</p>
+              <p><strong>Username:</strong> {user.username}</p>
+              <p><strong>Email:</strong> {user.email}</p>
+              <p><strong>Teléfono:</strong> {user.phone}</p>
+              <p><strong>Website:</strong> {user.website}</p>
+              <p><strong>Dirección:</strong> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
+              <p><strong>Compañía:</strong> {user.company.name}</p>
+              <p><strong>CatchPhrase:</strong> {user.company.catchPhrase}</p>
+              <p><strong>BS:</strong> {user.company.bs}</p>
+            </div>
+          )}
+          <button onClick={handleGenerateNewQR} className="button">
+            Generar Nuevo QR
+          </button>
+          <button onClick={handleBlockQR} className="button">
+            Bloquear QR
+          </button>
+          <button onClick={handlePrintQR} className="button">
+            Imprimir QR
+          </button>
+          {qrValue && !isBlocked(qrValue) && (
+            <div className="qr-code" style={qrStyle}>
+              <QRCode value={qrValue} />
+            </div>
+          )}
+          {qrValue && isBlocked(qrValue) && (
+            <div className="blocked-message">
+              <p>El acceso a esta URL está bloqueado.</p>
+            </div>
+          )}
         </div>
-      )}
-      <button onClick={handleGenerateNewQR} className="button">
-        Generar Nuevo QR
-      </button>
-      <button onClick={handleBlockQR} className="button">
-        Bloquear QR
-      </button>
-      <button onClick={handlePrintQR} className="button">
-        Imprimir QR
-      </button>
-      {qrValue && !isBlocked(qrValue) && (
-        <div className="qr-code" style={qrStyle}>
-          <QRCode value={qrValue} />
-        </div>
-      )}
-      {qrValue && isBlocked(qrValue) && (
-        <div className="blocked-message">
-          <p>El acceso a esta URL está bloqueado.</p>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
